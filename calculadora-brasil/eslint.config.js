1189d575b88244122a07501fb5def5b1e6ace215
josesan28 <san24092@uvg.edu.gt>
2026-05-26 19:49:18 -0600

    Configuración inicial del proyecto

---
+++ calculadora-brasil/eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
+
+export default [
+  { ignores: ['dist', 'node_modules', '.storybook', 'storybook-static'] },
+  {
+    files: ['src/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/react-in-jsx-scope': 'off',
+      semi: ['error', 'never'],
+      'max-len': ['error', { code: 120 }],
+      'no-unused-vars': ['warn'],
+    },
+  },
+]
